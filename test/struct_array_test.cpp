f9609933f8b25c62c54890e1e48ff313bd39fcf3
Yamahari <40495288+Yamahari@users.noreply.github.com>
2020-07-12 12:43:23 +0200

    add new test for swapping struct_array and its iterator

--- test/struct_array_test.cpp
+++ test/struct_array_test.cpp
@@ -15,17 +15,49 @@ struct bar {
 
 int main() {
     soa::struct_array<foo> sf;
-    soa::struct_array<bar> sb;
+    soa::struct_array<bar> sb0, sb1;
 
     for(auto i = 0; i < 10; ++i)
         sf.emplace_back(std::make_tuple(i));
 
-    std::transform(std::begin(sf), std::end(sf), std::back_inserter(sb),
-        [](const std::tuple<int&>& foo) noexcept {
-            const auto& [x] = foo;
+    std::cout << "sf:\n";
+    for(const auto& [x] : sf)
+        std::cout << x << '\n';
+    std::cout << '\n';
+
+    std::transform(std::begin(sf), std::end(sf), std::back_inserter(sb0),
+        [](const auto& v) noexcept 
+        {
+            const auto& [x] = v;
             return std::make_tuple(x, x * x);
         });
 
-    for(const auto& [x, y] : sb)
+    std::cout << "sb0 unsorted:\n";
+    for(const auto& [x, y] : sb0)
         std::cout << x << ' ' << y << '\n';
+    std::cout << '\n';
+
+    std::sort(std::begin(sb0), std::end(sb0), [](const auto& lhs, const auto& rhs) noexcept {
+        const auto& [x0, y0] = lhs;
+        const auto& [x1, y1] = rhs;
+        return x0 > x1 || (x0 == x1 && y0 > y1);
+    });
+
+    std::cout << "sb0 sorted:\n";
+    for(const auto& [x, y] : sb0)
+        std::cout << x << ' ' << y << '\n';
+    std::cout << '\n';
+
+    using std::swap;
+    swap(sb0, sb1);
+
+    std::cout << "sb0 swapped:\n";
+    for(const auto& [x, y] : sb0)
+        std::cout << x << ' ' << y << '\n';
+    std::cout << '\n';
+
+    std::cout << "sb1:\n";
+    for(const auto& [x, y] : sb1)
+        std::cout << x << ' ' << y << '\n';
+    std::cout << '\n';
 }
